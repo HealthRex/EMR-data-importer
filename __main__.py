355ad2612eeea8a58543071ea78c66609602928a
Rowan Copley <rowan.copley@gmail.com>
2020-09-30 21:35:10 -0700

    add main file

--- __main__.py
+++ __main__.py
@@ -2,11 +2,21 @@ from .emr_importer import Importer
 import json
 import os
 
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', help='the location of the config file', required=False)
+
 try:
-    with open(os.path.join(os.path.dirname(__file__),"config.json"), 'r') as f:
+    with open(args.config) as f:
         config = json.loads(f.read())
 except:
-        print("no config file found")
-imp = Importer(**config)
-path = imp.run()
-print("Results at", path)
+    try:
+        with open(os.path.join(os.path.dirname(__file__),"config.json"), 'r') as f:
+            config = json.loads(f.read())
+    except:
+            print("no config file found")
+finally:
+    imp = Importer(**config)
+    path = imp.run()
+    print("Results at", path)
