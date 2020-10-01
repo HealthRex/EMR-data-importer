300f816b5ba35e71b1ce73fc8af09efc47fb0e0b
Rowan Copley <rowan.copley@gmail.com>
2020-09-30 22:08:54 -0700

    only try running importer code if no errors

--- __main__.py
+++ __main__.py
@@ -6,17 +6,20 @@ import os
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', help='the location of the config file', required=False)
+args = parser.parse_args()
 
 try:
+    print("opening", args.config)
     with open(args.config) as f:
         config = json.loads(f.read())
 except:
+    print("didn't work, trying for default")
     try:
         with open(os.path.join(os.path.dirname(__file__),"config.json"), 'r') as f:
             config = json.loads(f.read())
-    except:
-            print("no config file found")
-finally:
+    except Exception as e:
+            raise Exception("No config file found in package directory or specified on command line") from e
+else:
     imp = Importer(**config)
     path = imp.run()
     print("Results at", path)
