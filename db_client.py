3815a6fc746c8b1f36b29911be317448f136c584
Rowan Copley <rowan.copley@gmail.com>
2020-10-08 21:58:35 -0700

    remove DB-specific packages from base requirements

--- db_client.py
+++ db_client.py
@@ -5,16 +5,21 @@ class Client():
     '''instantiates a generic Client object with a query method'''
     def __init__(self, database_type, credentials):
         self.type = database_type
-        if database_type == 'bigquery':
+        if self.type == 'bigquery':
             from google.cloud import bigquery
             os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials['gcloud_credentials']
             os.environ['GCLOUD_PROJECT'] = credentials['gcloud_project']
             self.client = bigquery.Client()
-
+        elif self.type == 'dummy':
+            self.client = None
         else:
             raise Exception(f"Database type '{database_type}' not supported")
 
     def query(self, sql):
         '''queries the database and returns a dataframe'''
-        query_job = self.client.query(sql)
-        return query_job.to_dataframe(progress_bar_type='tqdm')
+        if self.type == 'bigquery':
+            query_job = self.client.query(sql)
+            return query_job.to_dataframe(progress_bar_type='tqdm')
+        elif self.type == 'dummy':
+            from pandas import DataFrame
+            return DataFrame()
